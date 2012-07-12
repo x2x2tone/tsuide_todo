c39f60acdfab30f100fb17ecc9a08f9f4858774a
@2C <x2x2tone@gmail.com>
2012-07-12 18:11:59 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TsuideTodo::Application.config.secret_token = '06545d8d96604452e1a41bb51d79434681bcf8b7e80ee38a75ccd9e2b62b858d202a9b7a562933aff5528cd40183b6fc7d158f2ec8fdfcde935062351792c550'
